d3b88ce5c498b6fbe6a734c35959118952296610
Flall <119894671+Flall@users.noreply.github.com>
2023-04-27 11:48:53 +0200

    Basic Dictionary component set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by
+            <a
+              href="https://www.linkedin.com/in/francescalalli/"
+              target="_blank"
+              id="social-link"
+              rel="noreferrer"
+            >
+              Francesca Lalli
+            </a>
+            , open-sourced on
+            <a
+              href="https://github.com/Flall/dictionary-project"
+              target="_blank"
+              id="social-link"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            and hosted on
+            <a
+              href="https://dynamic-hummingbird-b6c74c.netlify.app"
+              target="_blank"
+              id="social-link"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
