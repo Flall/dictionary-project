996363868499a37c6fc9aebe0957f891a53d890b
Flall <119894671+Flall@users.noreply.github.com>
2023-04-28 10:44:27 +0200

    Phonetic component and functionality added

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -10,10 +9,10 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <strong>Example:</strong>
+              <strong>Example: </strong>
               <em>{definition.example}</em>
               <br />
               <Synonyms synonyms={definition.synonyms} />
